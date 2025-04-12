0f0c6fc826a3da64a5ee8245c743978bec597bdc
juIT <153582123+kjh5848@users.noreply.github.com>
2025-04-12 12:05:14 +0900

    feat: navigation, usePathname 설정 생성

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+    const path = usePathname()
+
     return (
         <ul>
-            <Link href="/">Home</Link>
-            <Link href="/about-us">About</Link>
+            <Link href="/"><li>Home{path === "/" ? "😊" : ""}</li></Link>
+            <Link href="/about-us"><li>About{path === "/about-us" ? "😊" : ""}</li></Link>
         </ul>
     )
 }
