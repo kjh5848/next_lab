560e8f4e15d7b52fa2cb30e07f1e4eaa32562f8c
juIT <153582123+kjh5848@users.noreply.github.com>
2025-04-13 12:35:18 +0900

    feat: movie.tsx 코드 수정정

--- components/movie.tsx
+++ components/movie.tsx
@@ -2,21 +2,22 @@ import Link from "next/link";
 import styles from "../styles/movie.module.css"
 
 
-interface IMovieProps {
-  title: string;
+interface MovieProps {
   id: string;
+  title: string;
   poster_path: string;
 }
 
-export default function Movie({ 
-    title, id, poster_path 
-}: 
-    IMovieProps 
-    ) {
+export default function Movie({ id, title, poster_path }: MovieProps) {
   return (
     <div className={styles.movie}>
-      <Link  href={`/movies/${id}`}> <img src={poster_path} alt={title} /></Link>
-      <Link href={`/movies/${id}`}>{title}</Link>
+      <Link 
+        href={`/movies/${id}`}
+        className={styles.link}
+      >
+        <img src={poster_path} alt={title} className={styles.poster} />
+        <h3 className={styles.title}>{title}</h3>
+      </Link>
     </div>
   );
 }
